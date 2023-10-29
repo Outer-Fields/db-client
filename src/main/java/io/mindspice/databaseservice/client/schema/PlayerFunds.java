7bfb487f1e87eea337c35a7b5e708c0e9de5f2ed
mindspice <admin@mindspice.io>
2023-10-29 06:25:54 -0400

    small typo on json property

--- src/main/java/io/mindspice/databaseservice/client/schema/PlayerFunds.java
+++ src/main/java/io/mindspice/databaseservice/client/schema/PlayerFunds.java
@@ -7,5 +7,5 @@ public record PlayerFunds(
         @JsonProperty("okra_token_amount") int okraTokens,
         @JsonProperty("potion_token_amount") int potionTokens,
         @JsonProperty("nft_drop_amount") int nftDrops,
-        @JsonProperty("outr_token_amounts") int outrTokens
+        @JsonProperty("outr_token_amount") int outrTokens
 ) { }
